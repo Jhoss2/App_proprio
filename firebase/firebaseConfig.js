df453515448b60d2cb922f3a2d1af7f361360331
Jhoss2 <Jhoss8631@gmail.com>
2026-04-07 20:16:55 +0000

    Update firebaseConfig.js

--- firebase/firebaseConfig.js
+++ firebase/firebaseConfig.js
@@ -1,6 +1,5 @@
 const { initializeApp, getApps } = require('firebase/app');
 const { getFirestore }           = require('firebase/firestore');
-const { getDatabase }            = require('firebase/database');
 
 const cfg = {
   apiKey:            "AIzaSyDJS5sgI7rFyAQAOZNgJsZ1nkWwjFI-cDE",
@@ -9,11 +8,9 @@ const cfg = {
   storageBucket:     "ninja-s-fries.firebasestorage.app",
   messagingSenderId: "187217291987",
   appId:             "1:187217291987:web:f15c681cb2db7a1af28226",
-  databaseURL:       "https://ninja-s-fries-default-rtdb.firebaseio.com",
 };
 
-const app  = getApps().length === 0 ? initializeApp(cfg) : getApps()[0];
-const db   = getFirestore(app);
-const rtdb = getDatabase(app);
+const app = getApps().length === 0 ? initializeApp(cfg) : getApps()[0];
+const db  = getFirestore(app);
 
-module.exports = { app, db, rtdb };
+module.exports = { app, db };
